9deb9ef464d86665425f84f6cca974ea3266d54f
sarthak247 <sarthak247@outlook.com>
2020-01-24 11:47:34 +0530

    add Download script

---
+++ 1. Data Download and Extract/donwload.sh
@@ -0,0 +1,4 @@
+# Download Dataset A
+wget https://archive.physionet.org/users/shared/challenge-2019/training_setA.zip
+# Download Dataset B
+wget https://archive.physionet.org/users/shared/challenge-2019/training_setB.zip
